c3c63d28b966aac34f9a7c9981da3e4a697bbfc7
Prezlyata Yuriy <Prezlyata Yuriy>
2018-11-08 18:42:55 +0200

    added scss

---
+++ src/component/App/app.component.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { data } from "../../config/data";
+import "./app.component.scss";
+
+class AppComponent extends Component {
+  render() {
+    console.log(data);
+    return (
+      <div className="App">
+        <h3>React APP</h3>
+        <h2>{data.eventDate.city}</h2>
+      </div>
+    );
+  }
+}
+
+export default AppComponent;
