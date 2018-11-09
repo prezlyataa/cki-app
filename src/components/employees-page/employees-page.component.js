55ce9a8cac502101e99382ad118c7557f2a7437e
Prezlyata Yuriy <Prezlyata Yuriy>
2018-11-09 17:37:52 +0200

    render list of employees according to data

--- src/components/employees-page/employees-page.component.js
+++ src/components/employees-page/employees-page.component.js
@@ -4,9 +4,26 @@ import { Layout } from "../layout";
 export class EmployeesPage extends Component {
   render() {
     const { data } = this.props;
+    console.log(data);
     return (
       <Layout>
         <h3>Employees page</h3>
+        <div>
+          {data.employees.map((employee, idx) => {
+            return (
+              <div key={idx}>
+                <img
+                  src={employee.photo}
+                  alt="photo"
+                  height="340px"
+                  width="220px"
+                />
+                <h4>{employee.name}</h4>
+                <p>{employee.info}</p>
+              </div>
+            );
+          })}
+        </div>
       </Layout>
     );
   }
